70e9d995d8fea813a4d3d3c4d67e12cc37f49f1d
Keith Mathe <keithmathe2002@gmail.com>
2022-02-26 15:16:21 -0500

    Added readme file and optional assignment challenges

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet var screen: UIView!
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        card.clipsToBounds = true
     }
 
 
+    @IBAction func didTapFlashcard(_ sender: Any) {
+        frontLabel.isHidden =  !frontLabel.isHidden
+    }
 }
 
